07cdbf65c295f577abf2cdf3b7678496de82b719
Sébastien GARCIA <sebastien.works@outlook.com>
2025-03-09 11:39:46 +0100

    Update Types.lua

--- Source/Types.lua
+++ Source/Types.lua
@@ -1,18 +1,4 @@
-export type LogEntry = {
-	Message: string,
-	Level: 'Info' | 'Warning' | 'Error',
-	Timestamp: number
-}
-
 export type LogType = 'Info' | 'Warning' | 'Error'
-
-export type LogService = {
-	FireLog: (self: LogService, message: string, level: string) -> (),
-	GetLogHistory: (self: LogService) -> {LogEntry},
-	ClearLogHistory: (self: LogService) -> (),
-	SetMaxLogEntries: (self: LogService, limit: number) -> (),
-	SetLogFilter: (self: LogService, levels: {string}) -> (),
-	LogToFile: (self: LogService, filename: string) -> (),
-}
+export type LogEntry = { Message: string, Level: LogType, Timestamp: number }
 
 return {}
